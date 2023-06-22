5d3545e52e1377927a26e5fe972327c54402b4d3
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-06-22 15:17:02 +0700

    metode if jika hanya 1 elemen / step 1

--- CircularQueues_149/CircularQueues_149.cpp
+++ CircularQueues_149/CircularQueues_149.cpp
@@ -48,6 +48,11 @@ public:
         }
         cout << "\n element deleted from teh queue is: " << queue_array[FRONT] << "\n";
 
+        //cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR) {
+            FRONT = -1;
+            REAR  = -1;
+        }
     }
 };
 
