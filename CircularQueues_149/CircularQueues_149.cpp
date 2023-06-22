5aba6c9bb9acec6ea6a5c908a5c35dd6fd793960
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-06-22 15:28:23 +0700

    metode iterasi daro front hingga rear

--- CircularQueues_149/CircularQueues_149.cpp
+++ CircularQueues_149/CircularQueues_149.cpp
@@ -71,6 +71,18 @@ public:
             cout << "QUeue is empty\n";
             return;
         }
+
+        cout << "\nelement is the queue are...\n";
+
+        // jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+        if (FRONT_position <= REAR_position) {
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+        
     }
 };
 
