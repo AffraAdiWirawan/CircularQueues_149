2b23002a79f6264a67e1f8404188b84c2d8fdfc9
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-06-22 15:15:36 +0700

    metode delete untuk front sama -1

--- CircularQueues_149/CircularQueues_149.cpp
+++ CircularQueues_149/CircularQueues_149.cpp
@@ -41,6 +41,12 @@ public:
     }
 
     void remove() {
+        //cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\n element deleted from teh queue is: " << queue_array[FRONT] << "\n";
 
     }
 };
